5a308ecc8e76b02c86f46a60cc40f0c7a19ca02f
nagma <bano.nagma.1@gmail.com>
2025-10-31 15:03:21 +0530

    createBrowserRouter and RouterProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,8 +20,34 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import EventsPage from "./pages/EventPage";
+import EventDetailsPage from "./pages/EventDetailsPage";
+import NewEventPage from "./pages/NewEventPage";
+import EditEventPage from "./pages/EditEventPage";
+import RootLayout from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <RootLayout />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "/events", element: <EventsPage /> },
+      { path: "/events/:eventId", element: <EventDetailsPage /> },
+      { path: "/events/new", element: <NewEventPage /> },
+      { path: "events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
