450e3f8ecc96c57105420c0165936f7f58312441
nagma <bano.nagma.1@gmail.com>
2025-10-31 19:41:08 +0530

    list of events

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+  { id: "e1", title: "Event-1" },
+  { id: "e2", title: "Event-2" },
+];
+
 function EventsPage() {
-  return <h1>Events Page</h1>;
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {EVENTS.map((eve) => (
+          <li key={eve.id}>
+            <Link to={`${eve.id}`}>{eve.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage;
