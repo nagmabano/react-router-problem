03fdf5bba59fa80edbb2a41aaf705d8770db443b
nagma <bano.nagma.1@gmail.com>
2025-11-01 20:11:51 +0530

    fetching data correctly

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -20,10 +20,10 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/eventsdddd");
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw new Response(JSON.stringify({message: "Could not fetch events.", status: 500}));
+    throw new Response(JSON.stringify({message: "Could not fetch events."}), ({status: 500}));
   } else {
     const resData = await response.json();
     return resData;
