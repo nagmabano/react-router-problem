d1945bbd1aa4a0a4a201973d7c144e9565b0aad2
nagma <bano.nagma.1@gmail.com>
2025-11-01 20:02:28 +0530

    Generic Error Handling. Extracting Error Data and Throwing responses

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -23,7 +23,7 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/eventsdddd");
 
   if (!response.ok) {
-    throw new Error("Could not fetch events");
+    throw new Response(JSON.stringify({message: "Could not fetch events.", status: 500}));
   } else {
     const resData = await response.json();
     return resData;
