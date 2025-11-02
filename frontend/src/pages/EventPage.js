d176518f8598518599fe38bebda99c44da018982
nagma <bano.nagma.1@gmail.com>
2025-11-02 18:28:59 +0530

    deffering data fetching

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -1,31 +1,48 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, Await } from "react-router-dom";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
-  const data = useLoaderData();
-  if(data.error) {
-    return <p>{data.message}</p>;
-  }
-
-  const events = data.events;
+  const { events } = useLoaderData();
 
   return (
-    <>
-      {<EventsList events={events} />}
-    </>
+    <Suspense fallback={<p style={{textAlign: 'center'}}>Loading..</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
+  // if(data.error) {
+  //   return <p>{data.message}</p>;
+  // }
+
+  // const events = data.events;
+
+  // return (
+  //   <>
+  //     {<EventsList events={events} />}
+  //   </>
+  // );
 }
 
 export default EventsPage;
 
-export async function loader() {
+export async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw new Response(JSON.stringify({message: "Could not fetch events."}), ({status: 500}));
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     const resData = await response.json();
-    return resData;
+    return resData.events;
   }
+}
+
+export function loader() {
+  return {
+    events: loadEvents()
+  };
 }
