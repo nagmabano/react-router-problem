af3cc4d3613c844b8f7a08417852be251599bd8b
nagma <bano.nagma.1@gmail.com>
2025-11-02 23:43:47 +0530

    controlling awaited and deffered data

--- frontend/src/pages/EventDetailsPage.js
+++ frontend/src/pages/EventDetailsPage.js
@@ -1,29 +1,63 @@
-import { redirect, useRouteLoaderData,
+import { redirect, useRouteLoaderData, Await
   // useParams, 
    } from "react-router-dom";
 import  EventItem  from "../components/EventItem";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventDetailsPage() {
-  const data = useRouteLoaderData('event-details');
+  const {event, events} = useRouteLoaderData('event-details');
   // const params = useParams();
   return (
     <>
-      <EventItem event={data.event} />
+    <Suspense fallback={<p style={{textAlign: "center"}}>Loading...</p>}>
+    <Await resolve={event}>
+        {(loadedEvent) => <EventItem event={loadedEvent} />}
+      </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{textAlign: "center"}}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents}/>}
+      </Await>
+    </Suspense>
     </>
   );
 }
 
 export default EventDetailsPage;
 
-export async function loader({request, params}) {
-  const id = params.eventId;
+async function loadEvent(id) {
+  // const id = params.eventId;
   const response = await fetch('http://localhost:8080/events/' + id);
   if (!response.ok) {
     throw JSON.stringify({message: "Could not fetch details for selected event."}, {
       status: 500
     })
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
+  }
+}
+
+export async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export async function loader({request, params}) {
+  const id = params.eventId;
+  
+  return {
+    event: await loadEvent(id),
+    events: loadEvents(),
   }
 }
 
